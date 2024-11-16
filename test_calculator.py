e84059f0386760448772e94822c2e35e3401a993
Doha2121 <142715163+Doha2121@users.noreply.github.com>
2024-11-16 17:29:57 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -6,3 +6,8 @@ class TestCalculator(unittest.TestCase):
       self.assertEqual(Calculator().add(3, 7), 10)
       self.assertEqual(Calculator().add(-1, -1), -2)
       self.assertEqual(Calculator().add(-1, 1), 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()

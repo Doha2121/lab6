44cda63c1e2c1f7cbaf0afbdc1af5b958f9b35d3
Doha2121 <142715163+Doha2121@users.noreply.github.com>
2024-11-16 15:56:38 +0200

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,8 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+      self.assertEqual(Calculator().add(3, 7), 10)
+      self.assertEqual(Calculator().add(-1, -1), -2)
+      self.assertEqual(Calculator().add(-1, 1), 0)
